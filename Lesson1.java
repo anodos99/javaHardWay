96c6f1c9ae7ae0a736cedb24035b9d2be9da6ea0
Joshua Bone <joshuabone@Joshuas-MacBook-Air.local>
2016-09-17 11:34:50 -0400

    add lesson 1

---
+++ Lesson1.java
@@ -0,0 +1,6 @@
+public class Lesson1{
+  public static void main(String[] args){
+    System.out.println("Joshua Bone is determined to learn how to code.");
+    System.out.println("Today's date is 9/17/16");
+  }
+}
